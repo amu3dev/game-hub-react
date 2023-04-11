bc29281604d4c0f475dd8043a3a38950e3beb796
Ahmed Yosry <amu3dev@gmail.com>
2023-04-11 16:12:31 +0200

    Build ColorModeSwitch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       {/* <Image src="https://via.placeholder.com/150" alt="Logo" /> */}
       {/* <Image
         src="https://bit.ly/dan-abramov"
@@ -12,7 +13,8 @@ const Navbar = () => {
       /> */}
 
       <Image src={logo} boxSize="60px" objectFit="cover" />
-      <Text> Nav</Text>
+      {/* <Text> Nav</Text> */}
+      <ColorModeSwitch />
     </HStack>
   );
 };
